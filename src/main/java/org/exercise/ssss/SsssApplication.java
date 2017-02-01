9919cbab3e0818f808e2eb8f9ef71c4bc2c1d1a0
Gabriel Rivera Safadi <gabriel.rivera.safadi@gmail.com>
2017-02-01 10:46:06 -0400

    Add main class and configuration.

---
+++ src/main/java/org/exercise/ssss/SsssApplication.java
@@ -0,0 +1,12 @@
+package org.exercise.ssss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsssApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsssApplication.class, args);
+    }
+}
